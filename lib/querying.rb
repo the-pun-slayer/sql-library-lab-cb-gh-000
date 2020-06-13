d07cfe94bc5d10f66f061a3b90e58dc0dc9c51dc
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-13 06:02:01 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.years FROM books join series ON series.id=books.series_id ORDER BY years ASC"
+  "SELECT books.title, books.year FROM books join series ON series.id=books.series_id ORDER BY years ASC"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
