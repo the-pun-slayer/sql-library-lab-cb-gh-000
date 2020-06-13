145d71d105cea617c9f269c6389f0244b6f2b0f4
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-13 06:05:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species) FROM characters GROUP BY species"
 end
 
 def select_name_and_series_subgenres_of_authors
