fa52c33b0065295cbe581e803ca97d9505fe9824
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-13 06:26:59 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,11 +12,11 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name,subgenres.name FROM authors JOIN series ON authors.id=series.author_id INNNER JOIN subgenres on series.subgenre_id=subgenres.id  "
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title, authors.name, characters.species FROM series JOIN authors ON series.author_id=authors.id INNER JOIN characters ON authors.id = characters.author_id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(characters.species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
